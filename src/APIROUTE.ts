06bddd8faa19c6011dcc0eb3b8c7f5f5e0da5281
Salim khadir <63880607+SalimKH5@users.noreply.github.com>
2024-08-30 00:10:01 +0200

    add movies and series to app

--- src/APIROUTE.ts
+++ src/APIROUTE.ts
@@ -1,10 +1,19 @@
 const BASE_POKEMON_URL=process.env.POKEMON_URL
 const BASE_MOVIE_URL=process.env.MOVIE_URL
 
+const movies=BASE_MOVIE_URL+"/api/movies"
 
+const movie_detail=BASE_MOVIE_URL+"/infos/movies/:id"
+const series=BASE_MOVIE_URL+"/api/series"
+const serie_detail=BASE_MOVIE_URL+"/infos/series/:id"
 
 
 export default {
     BASE_MOVIE_URL,
-    BASE_POKEMON_URL
+    BASE_POKEMON_URL,
+    movies,
+    movie_detail,
+    serie_detail,
+    series,
+   
 }
